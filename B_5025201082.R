2d5b5d1f11929bfc122689ed7a0ee0e8b7870fd6
Farrel Emerson <82019030+Dvizio@users.noreply.github.com>
2022-05-29 23:21:18 +0700

    Update B_5025201082.R

--- B_5025201082.R
+++ B_5025201082.R
@@ -88,6 +88,49 @@ qchisq(p = 0.05, df = 2, lower.tail=FALSE)
 # Dengan tingkat keyakinan 95%, diyakini bahwa tidak terdapat 
 # perbedaan rata-rata saham pada perusahaan di Bandung dan Bali.
 
+# Soal 4
+## Soal 4A
+dataoneway <- read.table("data_soal_4.txt", h = T)
+attach(dataoneway)
+names(dataoneway)
+
+dataoneway$Group <- as.factor(dataoneway$Group)
+dataoneway$Group = factor(dataoneway$Group,labels = c("Kucing Oren", "Kucing Hitam", "Kucing Putih"))
+
+class(dataoneway$Group)
+
+Group1 <- subset(dataoneway, Group == "Kucing Oren")
+Group2 <- subset(dataoneway, Group == "Kucing Hitam")
+Group3 <- subset(dataoneway, Group == "Kucing Putih")
+
+qqnorm(Group1$Length)
+qqline(Group1$Length)
+
+qqnorm(Group2$Length)
+qqline(Group2$Length)
+
+qqnorm(Group3$Length)
+qqline(Group3$Length)
+
+## Soal 4B
+bartlett.test(Length ~ Group, data = dataoneway)
+
+## Soal 4C
+model1 = lm(Length ~ Group, data = dataoneway)
+anova(model1)
+
+## Soal 4D
+## Jawaban terlampir di laporan
+
+## Soal 4E
+TukeyHSD(aov(model1))
+
+## Soal 4F
+install.packages("ggplot2")
+library("ggplot2")
+
+ggplot(dataoneway, aes(x = Group, y = Length)) + geom_boxplot(fill = "grey80", colour = "black") + scale_x_discrete() + xlab("Treatment Group") + ylab("Length (cm)")
+
 
 
 # Soal 5
